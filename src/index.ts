22b339639dc4b32b349058220d955764a85a9d24
Gilles Zunino <gzunino@hotmail.com>
2021-05-09 17:03:39 -0700

    First plugin implementation - Exports as HAR and display the raw json in a dialog

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------------
+// Copyright 2021, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+const requestActions = [{
+	label: "Pretty print last request/response",
+	action: async (context: any, data: any): Promise<void> =>{
+		const { request, requestGroup } = data;
+		      const har:string = await context.data.export.har( { includePrivate: true } );
+			  const html: string = `<code>${har}</code>`;
+		      context.app.showGenericModalDialog('Results', { html });
+			}
+}];
+
+
+export { requestActions }
