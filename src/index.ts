9ee7181a4eb78500fcdf88bfbade21e596e2c694
Gilles Zunino <gzunino@hotmail.com>
2021-05-09 17:42:14 -0700

    Prefer 'app.dialog()' over 'app.showGenericModalDialog()'

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ const requestActions = [{
 	action: async (context: any, data: any): Promise<void> =>{
 		const { request, requestGroup } = data;
 		      const har:string = await context.data.export.har( { includePrivate: true } );
-			  const html: string = `<code>${har}</code>`;
-		      context.app.showGenericModalDialog('Results', { html });
+
+			  const body: HTMLDivElement = document.createElement("div");
+			  body.innerHTML = `<code>${har}</code>`;
+
+			  context.app.dialog(`Pretty Print - "${request.name}"`, body, { tall: true, wide: false, skinny: false });
 			}
 }];
 
