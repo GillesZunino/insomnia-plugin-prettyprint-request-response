542838ada4ff2e94e1e989c4beb4ee6bf0a2fee3
Gilles Zunino <gzunino@hotmail.com>
2021-05-09 17:22:33 -0700

    Customize command icon

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 const requestActions = [{
 	label: "Pretty print last request/response",
+	icon: "fa-file-text-o",
 	action: async (context: any, data: any): Promise<void> =>{
 		const { request, requestGroup } = data;
 		      const har:string = await context.data.export.har( { includePrivate: true } );
