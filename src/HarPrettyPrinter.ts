9d5ae1d24a81babbc28a67f0d21e01df764e7629
Gilles Zunino <gzunino@hotmail.com>
2021-05-10 21:11:08 -0700

    Fix empty response header formatted incorrectly ('Referrer-Policy: :')

--- src/HarPrettyPrinter.ts
+++ src/HarPrettyPrinter.ts
@@ -20,13 +20,30 @@ export default class HarPrettyPrinter {
 
     public toHtml(requestName: string, rawhar: string, templateName: string): string {
         const har: any = JSON.parse(rawhar);
-        const entry: any = har.log.entries.find((entry: any) => entry.comment === requestName);
+        const entry: any = this.cleanupHarEntryHeaders(har.log.entries.find((entry: any) => entry.comment === requestName));
         return this.htmlTemplatingEnvironment.render(templateName, { request: entry.request, response: entry.response });
     }
 
     public toText(requestName: string, rawhar: string, templateName: string): string {
         const har: any = JSON.parse(rawhar);
-        const entry: any = har.log.entries.find((entry: any) => entry.comment === requestName);
+        const entry: any = this.cleanupHarEntryHeaders(har.log.entries.find((entry: any) => entry.comment === requestName));
         return this.textTemplatingEnvironment.render(templateName, { request: entry.request, response: entry.response });
+    }
+
+    private cleanupHarEntryHeaders(harEntry: any): any {
+        // Insomnia sometimes exports response headers incorrectly. For instance, "Referrer-Policy: " gets exported as follows:
+        // {
+        //     "name": "Referrer-Policy: ",
+        //     "value": ""
+        // }
+        // We traverse all headers, trim whitespaces and remove the trailing ':' from the header name
+        for (let responseHeaderIndex: number = 0; responseHeaderIndex < harEntry.response.headers.length; responseHeaderIndex++) {
+            let currentHeader: any = harEntry.response.headers[responseHeaderIndex];
+            let trimmedHeaderName: string = currentHeader.name.trimEnd();
+            if (trimmedHeaderName[trimmedHeaderName.length - 1] === ':') { trimmedHeaderName = trimmedHeaderName.slice(0, -1); }
+            currentHeader.name = trimmedHeaderName;
+        }
+
+        return harEntry;
     }
 }
