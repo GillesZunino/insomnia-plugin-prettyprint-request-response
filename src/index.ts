b712242ba736f34aeb4d1aafecf2f3a63ff73b5b
Gilles Zunino <gzunino@hotmail.com>
2021-05-30 13:46:52 -0700

    Catch failures to export requests as HAR and display a friendly error message

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,22 @@ const requestActions = [{
 	icon: "fa-file-text-o",
 	action: async (context: any, data: any): Promise<void> => {
 		const { request, requestGroup } = data;
-		const har: string = await context.data.export.har( { includePrivate: true } );
 
-		const harPrettyPrinter: HarPrettyPrinter = new HarPrettyPrinter();
-		let html: string = harPrettyPrinter.toHtml(request.name, har, "fiddler.njk");
-		const plainText: string = harPrettyPrinter.toText(request.name, har, "plaintext.njk");
+		try {
+			const har: string = await context.data.export.har( { includePrivate: true } );
 
-		clipboard.write({
-			text: plainText,
-			html: html
-		});
+			const harPrettyPrinter: HarPrettyPrinter = new HarPrettyPrinter();
+			let html: string = harPrettyPrinter.toHtml(request.name, har, "fiddler.njk");
+			const plainText: string = harPrettyPrinter.toText(request.name, har, "plaintext.njk");
+	
+			clipboard.write({
+				text: plainText,
+				html: html
+			});
+		}
+		catch (e) {
+			await context.app.alert("Requests could not be exported as HAR", e.message);
+		}
 	}
 }];
 
